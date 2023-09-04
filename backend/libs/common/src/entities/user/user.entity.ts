4f7a5e61cd02ef4b75e2b68f0a97348000c07644
Notaovercat <fdoge@list.ru>
2023-09-04 19:11:24 +0600

    added desc

--- backend/libs/common/src/entities/user/user.entity.ts
+++ backend/libs/common/src/entities/user/user.entity.ts
@@ -3,6 +3,7 @@ import { ROLE, User } from '@prisma/client';
 export class UserEntity implements User {
   id: string;
   username: string;
+  description: string | null;
   email: string;
   password: string;
   role: ROLE;

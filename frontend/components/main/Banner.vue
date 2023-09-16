99e7c87398be0128416df0cd8967b5cde62ee522
Notaovercat <fdoge@list.ru>
2023-09-16 13:55:37 +0600

    new images

--- frontend/components/main/Banner.vue
+++ frontend/components/main/Banner.vue
@@ -1,4 +1,8 @@
 <template>
-  <img class="banner w-full object-fit h-56" />
+  <img
+    src="../../assets/images/banner.png"
+    class="object-cover md:object-contain max-w-full rounded h-56"
+    alt="banner"
+  />
 </template>
 <!-- src="https://media.discordapp.net/attachments/997479667802066965/1141750948390244432/image.png?width=1439&height=522" -->

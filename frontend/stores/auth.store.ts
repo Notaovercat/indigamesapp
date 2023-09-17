94e9b406b8f4e7cf13b590e9362c3d370617d039
Notaovercat <fdoge@list.ru>
2023-09-17 15:44:50 +0600

    removed axios

--- frontend/stores/auth.store.ts
+++ frontend/stores/auth.store.ts
@@ -1,12 +1,7 @@
-import axios, { AxiosError } from "axios";
 import { ICredentionals } from "@/types/user/creds.interface";
 import { ICreateUser } from "@/types/user/createUser.interface";
 
 export const useAuth = defineStore("auth", () => {
-  // define config to retreive api url
-  const config = useRuntimeConfig();
-  const apiUrl = config.public.API_URL;
-
   // error message for auth methods
   const errorMsg = ref("");
 
@@ -17,39 +12,70 @@ export const useAuth = defineStore("auth", () => {
   const isAuthed = ref(false);
 
   async function logIn(creds: ICredentionals) {
-    try {
-      errorMsg.value = "";
-      await axios.post(`${apiUrl}/auth/login`, creds, {
-        withCredentials: true,
-      });
+    errorMsg.value = "";
+    const { data, error } = await useMyFetch("auth/login", {
+      method: "POST",
+      body: creds,
+    });
 
-      // update userid
-      await checkIfLoggedIn();
-
-      //redirect to the main page
-      return navigateTo("/");
-    } catch (e) {
-      if (e instanceof AxiosError) {
-        if (e.response?.status === 401)
-          errorMsg.value = "Wrong email or password";
-        else errorMsg.value = "Server error, try again later";
-      }
+    if (error.value) {
+      if (error.value.statusCode === 401)
+        errorMsg.value = "Wrong email or password";
+      else errorMsg.value = "Server error, try again later";
     }
+
+    if (data.value) {
+      await checkIfLoggedIn();
+      return navigateTo("/");
+    }
+    // try {
+    //   errorMsg.value = "";
+    //   await axios.post(`${apiUrl}/auth/login`, creds, {
+    //     withCredentials: true,
+    //   });
+
+    //   // update userid
+    //   await checkIfLoggedIn();
+
+    //   //redirect to the main page
+    //   return navigateTo("/");
+    // } catch (e) {
+    //   if (e instanceof AxiosError) {
+    //     if (e.response?.status === 401)
+    //       errorMsg.value = "Wrong email or password";
+    //     else errorMsg.value = "Server error, try again later";
+    //   }
+    // }
   }
 
-  async function signUp(data: ICreateUser) {
-    try {
-      errorMsg.value = "";
-      await axios.post(`${apiUrl}/auth/register`, data);
-      //redirect to the main page
-      return navigateTo({ name: "login" });
-    } catch (e) {
-      if (e instanceof AxiosError) {
-        if (e.response?.status === 400)
-          errorMsg.value = "This email already in use";
-        else errorMsg.value = "Server error, try again later";
-      }
+  async function signUp(input: ICreateUser) {
+    errorMsg.value = "";
+    const { data, error } = await useMyFetch("auth/register", {
+      method: "POST",
+      body: input,
+    });
+
+    if (error.value) {
+      if (error.value.statusCode === 400)
+        errorMsg.value = "This email already in use";
+      else errorMsg.value = "Server error, try again later";
     }
+
+    if (data.value) {
+      return navigateTo({ name: "login" });
+    }
+    // try {
+    //   errorMsg.value = "";
+    //   await axios.post(`${apiUrl}/auth/register`, data);
+    //   //redirect to the main page
+    //   return navigateTo({ name: "login" });
+    // } catch (e) {
+    //   if (e instanceof AxiosError) {
+    //     if (e.response?.status === 400)
+    //       errorMsg.value = "This email already in use";
+    //     else errorMsg.value = "Server error, try again later";
+    //   }
+    // }
   }
 
   async function logOut() {

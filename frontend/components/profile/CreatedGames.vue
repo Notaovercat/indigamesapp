409b2f8b965b79ad6143455e1f5a9bef1d0ba9f2
Notaovercat <fdoge@list.ru>
2023-09-27 20:05:04 +0600

    fixed interface

--- frontend/components/profile/CreatedGames.vue
+++ frontend/components/profile/CreatedGames.vue
@@ -5,31 +5,29 @@ const errorMsg = computed(() => profileStore.gameErrorMsg);
 const isLoading = computed(() => profileStore.gameLoading);
 </script>
 <template>
-  <div class="games-container flex flex-col gap-2 px-2 w-full">
-    <div v-if="errorMsg" class="game-error w-full text-xl text-center">
+  <div class="flex flex-col gap-2 px-2 w-full">
+    <div v-if="errorMsg" class="w-full text-xl text-center">
       <p>{{ errorMsg }}</p>
     </div>
-    <div
-      v-else-if="isLoading"
-      class="game-loading flex justify-center items-center w-full"
-    >
+    <div v-else-if="isLoading" class="flex justify-center items-center w-full">
       <LoadingSpinner />
     </div>
-    <ProfileGameCard
-      v-else-if="data.length > 0"
-      v-for="game of data"
-      :game="game"
-      :key="game.id"
-      @click="
-        navigateTo({
-          name: game.isVisible ? 'game-id' : 'my-game-id',
-          params: { id: game.id },
-        })
-      "
-    />
+    <div class="flex flex-col gap-3" v-else-if="data.length > 0">
+      <ProfileGameCard
+        v-for="game of data"
+        :game="game"
+        :key="game.id"
+        @click="
+          navigateTo({
+            name: game.isVisible ? 'game-id' : 'my-game-id',
+            params: { id: game.id },
+          })
+        "
+      />
+    </div>
     <div
       v-else
-      class="game-empty flex items-center justify-center w-full py-9 text-3xl font-bold"
+      class="flex items-center justify-center w-full py-9 text-3xl font-bold"
     >
       <p>There are no games</p>
     </div>
